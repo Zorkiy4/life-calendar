a470318b41f1160e932fec0e21dad824e3e8c3e1
Aleksandr Kokin <aleksandr.kokin@intapp.com>
2023-09-03 11:51:18 +0200

    Make generate_two_years_calendar.py scalable

--- generate_two_years_calendar.py
+++ generate_two_years_calendar.py
@@ -6,24 +6,25 @@ from generate_year_calendar import generate_year_calendar
 
 def merge_images(image1, image2, offset_x = 0, offset_y = 0):
     image1_size = image1.size
-    new_image = Image.new('RGB',(image1_size[0] + image2.size[0] + offset_x, image1_size[1]), (250,250,250))
+    new_image = Image.new('RGB',(image1_size[0] + image2.size[0] + offset_x, image1_size[1]), (255,255,255))
     new_image.paste(image1,(0,0))
     new_image.paste(image2,(image1_size[0] + offset_x, offset_y))
     return new_image
 
-cell_size=25
+year_calendar_cell_size=25
+life_calendar_cell_size=10
 
 current_year = datetime.datetime.now().year
-current_year_calendar_image = generate_year_calendar(current_year, cell_size)
+current_year_calendar_image = generate_year_calendar(current_year, year_calendar_cell_size)
 
 next_year = current_year + 1
-next_year_calendar_image = generate_year_calendar(next_year, cell_size)
+next_year_calendar_image = generate_year_calendar(next_year, year_calendar_cell_size)
 
 two_years_image = merge_images(current_year_calendar_image, next_year_calendar_image)
 
-life_calendar = generate_life_calendar()
+life_calendar = generate_life_calendar(cell_size = life_calendar_cell_size, index_size = life_calendar_cell_size)
 
-all_calendars_image = merge_images(two_years_image, life_calendar, offset_y = 40)
+all_calendars_image = merge_images(two_years_image, life_calendar, offset_y = year_calendar_cell_size * 2)
 
 all_calendars_image.save("my_life_calendar.png")
 all_calendars_image.show()
